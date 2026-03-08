65ecf616fd7be58ec496cdf72a45c3a72ac543b8
Sadia Moeed <sadiamoeed13@gmail.com>
2026-03-08 15:50:30 +0500

    Prepare Vite Tic Tac Toe for Heroku

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import {useState} from 'react';
+import "./App.css";
+
+
+type squareProp = {
+ value : 'X' | 'O' | null
+ onSquareClick: () => void
+}
+
+function Square({value , onSquareClick}: squareProp) {
+
+ return (
+ <button className="square" onClick = {onSquareClick} >
+ {value}
+
+ </button>
+ )
+}
+
+export default function Board(){
+
+ const [xIsNext,setxIsNext] = useState(true)
+ const [squares,setSquares] = useState(Array(9).fill(null))
+ const winner = calculateWinner(squares);
+ let status;
+ if (winner) {
+ status = "Winner: " + winner;
+ } else {
+ status = "Next player: " + (xIsNext ? "X" : "O");
+ }
+
+ function handleClick (i: number){
+ const nextSquare = squares.slice();
+ if(squares[i] || calculateWinner(squares)){
+ return
+ };
+ if (xIsNext) {
+ nextSquare[i] = 'X';
+ }
+ else {
+ nextSquare[i] = 'O';
+ }
+ setSquares(nextSquare);
+ setxIsNext(!xIsNext)
+
+ }
+
+ return (
+
+ <>
+ <div className="status">{status}</div>
+ <div className='board-row'>
+ <Square value = {squares[0]} onSquareClick= {()=> handleClick(0)} />
+ <Square value ={squares[1] } onSquareClick={()=> handleClick(1)} />
+ <Square value = {squares[2]} onSquareClick={()=> handleClick(2)} />
+ </div>
+
+ <div className='board-row'>
+ <Square value = {squares[3]} onSquareClick={()=> handleClick(3)}/>
+ <Square value = {squares[4]} onSquareClick={()=> handleClick(4)}/>
+ <Square value = {squares[5]} onSquareClick={()=> handleClick(5)}/>
+ </div>
+
+ <div className='board-row'>
+ <Square value = {squares[6]} onSquareClick={()=> handleClick(6)}/>
+ <Square value = {squares[7]} onSquareClick={()=> handleClick(7)}/>
+ <Square value = {squares[8]}onSquareClick={()=> handleClick(8)}/>
+ </div>
+
+</>
+
+ )
+}
+
+type Cell = "X" | "O" | null
+type board = ReadonlyArray<Cell>;
+function calculateWinner(squares : board) {
+ const lines = [
+ [0, 1, 2],
+ [3, 4, 5],
+ [6, 7, 8],
+ [0, 3, 6],
+ [1, 4, 7],
+ [2, 5, 8],
+ [0, 4, 8],
+ [2, 4, 6]
+ ];
+ for (let i = 0; i < lines.length; i++) {
+ const [a, b, c] = lines[i];
+ if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+ return squares[a];
+ }
+ }
+ return null;
+}
